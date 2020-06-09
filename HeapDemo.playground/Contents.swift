ab74bd23d11f1336acfd748a4f041305a0d1c1e9
pitt500 <pedro.rojaslo50@gmail.com>
2020-06-09 06:02:29 -0700

    Add Heap Demo for max and min heap priority

--- HeapDemo.playground/Contents.swift
+++ HeapDemo.playground/Contents.swift
@@ -1,2 +1,16 @@
 import Foundation
 
+let array = [1, 12, 3, 4, 1, 6, 8, 7]
+var maxHeap = Heap(sort: >, elements: array)
+var minHeap = Heap(sort: <, elements: array)
+
+print("Max heap:")
+while !maxHeap.isEmpty {
+  print(maxHeap.remove()!)
+}
+
+print("\nMin heap: ")
+while !minHeap.isEmpty {
+  print(minHeap.remove()!)
+}
+
